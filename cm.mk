60928c6488be0de06e80a5eb0e68c4d31261a37c
ZIM <zim555@mail.ru>
2016-11-17 20:58:25 +0200

    Fix path for fortuna3g

--- cm.mk
+++ cm.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/fortunave3g/full_fortuna3g.mk)
+$(call inherit-product, device/samsung/fortuna3g/full_fortuna3g.mk)
 
 # Setup device configuration
 PRODUCT_DEVICE := fortuna3g
