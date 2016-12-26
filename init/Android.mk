e555dd8a64eae36112fa3d8f2289fbf28113b300
Carlos Arriaga <carlosarriagacm@gmail.com>
2016-12-25 18:23:52 -0600

    minnor update

--- init/Android.mk
+++ init/Android.mk
@@ -4,8 +4,6 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
-LOCAL_CFLAGS := -Wall
+LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_SRC_FILES := init_fortuna3g.cpp
 LOCAL_MODULE := libinit_fortuna3g
-
-include $(BUILD_STATIC_LIBRARY)
