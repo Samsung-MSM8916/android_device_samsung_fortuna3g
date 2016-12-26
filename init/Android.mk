7c10e66fd2b04d5d52ccb321b4b217c351440db3
Carlos Arriaga <carlosarriagacm@gmail.com>
2016-12-25 18:59:39 -0600

    fix

--- init/Android.mk
+++ init/Android.mk
@@ -7,3 +7,5 @@ LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_SRC_FILES := init_fortuna3g.cpp
 LOCAL_MODULE := libinit_fortuna3g
+
+include $(BUILD_STATIC_LIBRARY)
