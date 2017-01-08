28e2e2eb81ccb2b397fd845aaab02fe60b188b7b
CarlosArriagaCM <CarlosArriagaCM@gmail.com>
2017-01-08 14:16:24 -0600

    AOSP

--- aosp_fortuna3g.mk
+++ aosp_fortuna3g.mk
@@ -1,9 +1,6 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/samsung/fortuna3g/full_fortuna3g.mk)
 
 # Setup device configuration
 PRODUCT_DEVICE := fortuna3g
-PRODUCT_NAME := cm_fortuna3g
+PRODUCT_NAME := aosp_fortuna3g
