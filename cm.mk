152967474274ddd4a6a8798e75e8e23f4ddbf429
ZIM <zim555@mail.ru>
2017-03-03 21:59:06 +0200

    Revert "Rebrand to lineageOS"
    This reverts commit 7a450f0fee588bdb6b686a67b04a4e7e4a4c8dfe.

--- cm.mk
+++ cm.mk
@@ -1,4 +1,4 @@
-# Inherit some common lineage stuff.
+# Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
@@ -6,4 +6,4 @@ $(call inherit-product, device/samsung/fortuna3g/full_fortuna3g.mk)
 
 # Setup device configuration
 PRODUCT_DEVICE := fortuna3g
-PRODUCT_NAME := lineage_fortuna3g
+PRODUCT_NAME := cm_fortuna3g
